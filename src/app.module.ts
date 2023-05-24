cce811f93560d389b8080ec572737fe2688682c1
Luis María García Salado <luiggi95@hotmail.com>
2023-05-24 13:03:09 -0300

    Add mongoose coneection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'../public'),
+    }),
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon')
+    ],
 })
 export class AppModule {}
---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,5 @@
+import { isInt16Array } from "util/types";
+
+export class CreatePokemonDto {
+
+}
